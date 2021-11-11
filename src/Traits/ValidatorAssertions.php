41f6bdec690404540c43553e082ee8b50f854c80
Daniel de Wit <daniel@danieldewit.nl>
2021-11-11 16:42:47 +0100

    wip

--- src/Traits/ValidatorAssertions.php
+++ src/Traits/ValidatorAssertions.php
@@ -2,26 +2,26 @@
 
 declare(strict_types=1);
 
-namespace DanielDeWit\LighthouseValidatorAssertions\Contracts;
+namespace DanielDeWit\LighthouseValidatorAssertions\Traits;
 
 use DanielDeWit\LighthouseValidatorAssertions\Assertions\AssertArrayContains;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Validation\Factory as ValidationFactory;
-use Illuminate\Foundation\Testing\TestCase;
 use Nuwave\Lighthouse\Support\Contracts\ArgumentSetValidation;
 
-/**
- * @mixin TestCase
- */
 trait ValidatorAssertions
 {
+    /**
+     * @throws BindingResolutionException
+     */
     protected function assertValidatorMessages(
         ArgumentSetValidation $validator,
         array $input,
         array $expected,
         string $msg = '',
     ): void {
-        $validationFactory = $this->app->make(ValidationFactory::class);
+        $validationFactory = $this->getApplication()->make(ValidationFactory::class);
 
         $errors = $validationFactory->make(
             $input,
@@ -37,4 +37,6 @@ trait ValidatorAssertions
             $msg,
         );
     }
+
+    abstract protected function getApplication(): Application;
 }
